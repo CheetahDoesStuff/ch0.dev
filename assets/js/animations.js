8999c6369d996f57400b04f6855c6ee54d52821e
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-12-08 17:00:45 +0100

    skills :O

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,6 +1,7 @@
 import { animate, spring } from 'https://esm.sh/animejs@4';
 
 document.addEventListener('DOMContentLoaded', () => {
+
     animate('nav', {
         y: [
             { to: '-100px', duration: 0 },
@@ -24,6 +25,30 @@ document.addEventListener('DOMContentLoaded', () => {
             { to: '40px', duration: 0 },
             { to: '0px', easing: 'easeOutQuad', duration: 750 }
         ],
-        delay: (el, i) => i * 200
+        delay: (el, i) => i * 200,
+        complete: () => {
+            animate('.link-group', {
+                opacity: [
+                    { to: '0', duration: 0 },
+                    { to: '1', easing: "easeInOutQuad", duration: 1000 }
+                ],
+                y: [
+                    { to: '40px', duration: 0 },
+                    { to: '0px', easing: 'easeOutQuad', duration: 750 }
+                ],
+                delay: (el, i) => i * 200 + 1000
+            });
+            animate('.skill', {
+                opacity: [
+                    { to: '0', duration: 0 },
+                    { to: '1', easing: "easeInOutQuad", duration: 1000 }
+                ],
+                y: [
+                    { to: '40px', duration: 0 },
+                    { to: '0px', easing: 'easeOutQuad', duration: 750 }
+                ],
+                delay: (el, i) => i * 200 + 400
+            });
+        }
     });
 });
